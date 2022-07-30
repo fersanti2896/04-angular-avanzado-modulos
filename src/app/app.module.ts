52d9edbb6179702963392cde6d64852c6c39b6c2
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-30 16:24:31 -0500

    Moviendo carpeta nopagefound a directorio raíz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
