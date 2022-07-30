fbe3f38907b819e53adc6503ed8c1f668aba9055
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-30 18:56:19 -0500

    Implementando ruta dashboard/{componente} en pages.routing.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 const routes: Routes = [
   /* path: /dashboard -> PagesRoutingModule */
   /* path: /auth -> AuthRoutingModule */
-  { path: '**', component: NopagefoundComponent } 
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent }
 ];
 
 @NgModule({
